9befc13ac2c0806941665cafb1b84549c66986bc
hakan.basturknewinn.com <forxynet@gmail.com>
2025-01-28 14:33:31 +0300

    updated hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,6 +4,7 @@ import Slider from 'react-slick'
 import Image1 from "../../assets/hero/headphone.png";
 import Image2 from "../../assets/category/vr.png";
 import Image3 from "../../assets/category/macbook.png";
+import Button from '../Shared/Button';
 
 
 const HeroData = [
@@ -12,7 +13,7 @@ const HeroData = [
     img: Image1,
     subtitle: "Beats Solo",
     title: "Wireless",
-    title2: "Virtual",
+    title2: "Headphone",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
   },
   {
@@ -56,20 +57,27 @@ export default function Hero() {
               HeroData.map((data) => (
                 <div key={data.id} className='grid grid-cols-1 sm:grid-cols-2'>
                   {/* text content section */}
-                  <div>
-                    <h1 className='text-2xl sm:text-6xl lg:text-2xl font-bold aos-init aos-animate'>{data.subtitle}</h1>
-                    <h1 className='text-5xl sm:text-6xl lg:text-7xl font-bold aos-init aos-animate'>{data.title}</h1>
-                    <h1 className='text-5xl uppercase text-white dark:text-white/5 sm:text-[80px] md:text-[100px] xl:text-[150px] font-bold aos-init aos-animate'>{data.title2}</h1>
+                  <div className='flex flex-col justify-center gap-4 sm:pl-3 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10'>
+                    <h1 className='text-2xl sm:text-6xl lg:text-2xl font-bold'>{data.subtitle}</h1>
+                    <h1 className='text-5xl sm:text-6xl lg:text-7xl font-bold'>{data.title}</h1>
+                    <h1 className='text-5xl uppercase text-white dark:text-white/5 sm:text-[80px] md:text-[100px] xl:text-[150px] font-bold'>{data.title2}</h1>
                     <div>
-                      <button className='bg-primary text-white cursor-pointer hover:scale-105 duration-300 py-2 px-8 rounded-full relative z-10'>ShopNow</button>
+                      <Button
+                        text="Shop By Category"
+                        bgColor="bg-primary"
+                        textColor="text-white" />
                     </div>
                   </div>
                   {/* img section */}
-                  <img
-                    src={data.img}
-                    alt=""
-                    className='w-[300px] sm:w-[450px] h-[300px] sm:h-[450px] sm:scale-105 lg:scale-120 object-contain mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)] relative z-40'
-                  />
+                  <div className='order-1 sm:order-2'>
+                    <div className='relative z-10 aos-init aos-animate'>
+                      <img
+                        src={data.img}
+                        alt=""
+                        className='w-[300px] sm:w-[450px] h-[300px] sm:h-[450px] sm:scale-105 lg:scale-120 object-contain mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)] relative z-40'
+                      />
+                    </div>
+                  </div>
                 </div>
               ))
             }
