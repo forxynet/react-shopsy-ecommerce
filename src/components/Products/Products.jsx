4b393f1007fed3cef7d7127ec91ec278d719dbfe
hakan.basturknewinn.com <forxynet@gmail.com>
2025-01-28 19:11:16 +0300

    added product

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,9 +1,91 @@
 import React from 'react'
+import Heading from '../Shared/Heading'
+import ProductCard from './ProductCard'
+
+// import images
+import Img1 from '../../assets/product/p-1.jpg';
+import Img2 from '../../assets/product/p-2.jpg';
+import Img3 from '../../assets/product/p-3.jpg';
+import Img4 from '../../assets/product/p-4.jpg';
+
+import Img5 from '../../assets/product/p-5.jpg';
+import Img6 from '../../assets/product/p-9.jpg';
+import Img7 from '../../assets/product/p-7.jpg';
+
+const ProductsData = [
+  {
+    id: 1,
+    img: Img1,
+    title: "Boat Headphone",
+    price: "120",
+    aeosDelay: "0"
+  },
+  {
+    id: 2,
+    img: Img2,
+    title: "Roky Mountain",
+    price: "420",
+    aeosDelay: "200"
+  },
+  {
+    id: 3,
+    img: Img3,
+    title: "Goggles",
+    price: "320",
+    aeosDelay: "400"
+  },
+  {
+    id: 4,
+    img: Img4,
+    title: "Printed",
+    price: "220",
+    aeosDelay: "60"
+  },
+]
+const ProductsData2 = [
+  {
+    id: 1,
+    img: Img5,
+    title: "Boat Headphone",
+    price: "120",
+    aeosDelay: "0"
+  },
+  {
+    id: 2,
+    img: Img6,
+    title: "Roky Mountain",
+    price: "420",
+    aeosDelay: "200"
+  },
+  {
+    id: 3,
+    img: Img7,
+    title: "Goggles",
+    price: "320",
+    aeosDelay: "400"
+  },
+  {
+    id: 4,
+    img: Img4,
+    title: "Printed",
+    price: "220",
+    aeosDelay: "60"
+  },
+]
 
 export default function Products() {
   return (
     <div>
-      Products
+      <div className="container">
+        {/* Header section */}
+        <Heading
+          title="Out Products"
+          subtitle={"Explore Our Products"}
+        />
+        {/* Body section */}
+        <ProductCard data={ProductsData} />
+        <ProductCard data={ProductsData2} />
+      </div>
     </div>
   )
 }
