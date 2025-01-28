e12d9aeeb05572444995d57f24236f96488eee7f
hakan.basturknewinn.com <forxynet@gmail.com>
2025-01-29 00:27:50 +0300

    added footer

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -49,39 +49,41 @@ export default function Hero() {
   };
 
   return (
-    <div className='container'>
-      <div className="overflow-hidden rounded-3xl min-h-[550px] sm:min-h-[650px] hero-bg-color flex justify-center items-center">
-        <div className='container pb-8 sm:pb-0'>
-          <Slider {...settings}>
-            {
-              HeroData.map((data) => (
-                <div key={data.id} className='grid grid-cols-1 sm:grid-cols-2'>
-                  {/* text content section */}
-                  <div className='flex flex-col justify-center gap-4 sm:pl-3 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10'>
-                    <h1 className='text-2xl sm:text-6xl lg:text-2xl font-bold'>{data.subtitle}</h1>
-                    <h1 className='text-5xl sm:text-6xl lg:text-7xl font-bold'>{data.title}</h1>
-                    <h1 className='text-5xl uppercase text-white dark:text-white/5 sm:text-[80px] md:text-[100px] xl:text-[150px] font-bold'>{data.title2}</h1>
-                    <div>
-                      <Button
-                        text="Shop By Category"
-                        bgColor="bg-primary"
-                        textColor="text-white" />
+    <div className='bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40'>
+      <div className='container'>
+        <div className="overflow-hidden rounded-3xl min-h-[550px] sm:min-h-[650px] hero-bg-color flex justify-center items-center">
+          <div className='container pb-8 sm:pb-0'>
+            <Slider {...settings}>
+              {
+                HeroData.map((data) => (
+                  <div key={data.id} className='grid grid-cols-1 sm:grid-cols-2'>
+                    {/* text content section */}
+                    <div className='flex flex-col justify-center gap-4 sm:pl-3 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10'>
+                      <h1 className='text-2xl sm:text-6xl lg:text-2xl font-bold'>{data.subtitle}</h1>
+                      <h1 className='text-5xl sm:text-6xl lg:text-7xl font-bold'>{data.title}</h1>
+                      <h1 className='text-5xl uppercase text-white dark:text-white/5 sm:text-[80px] md:text-[100px] xl:text-[150px] font-bold'>{data.title2}</h1>
+                      <div>
+                        <Button
+                          text="Shop By Category"
+                          bgColor="bg-primary"
+                          textColor="text-white" />
+                      </div>
+                    </div>
+                    {/* img section */}
+                    <div className='order-1 sm:order-2'>
+                      <div className='relative z-10 aos-init aos-animate'>
+                        <img
+                          src={data.img}
+                          alt=""
+                          className='w-[300px] sm:w-[450px] h-[300px] sm:h-[450px] sm:scale-105 lg:scale-120 object-contain mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)] relative z-40'
+                        />
+                      </div>
                     </div>
                   </div>
-                  {/* img section */}
-                  <div className='order-1 sm:order-2'>
-                    <div className='relative z-10 aos-init aos-animate'>
-                      <img
-                        src={data.img}
-                        alt=""
-                        className='w-[300px] sm:w-[450px] h-[300px] sm:h-[450px] sm:scale-105 lg:scale-120 object-contain mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)] relative z-40'
-                      />
-                    </div>
-                  </div>
-                </div>
-              ))
-            }
-          </Slider>
+                ))
+              }
+            </Slider>
+          </div>
         </div>
       </div>
     </div>
