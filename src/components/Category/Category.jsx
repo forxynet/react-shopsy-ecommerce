36cd9cfddd74497b1c920feecd2f7ec1042497d0
hakan.basturknewinn.com <forxynet@gmail.com>
2025-01-29 15:28:30 +0300

    updated banner

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -28,7 +28,7 @@ export default function Category() {
                 />
               </div>
             </div>
-            <img src={Image1} alt="" className='w-[320px] absolute bottom-0' />
+            <img data-aos="zoom-in" src={Image1} alt="" className='w-[320px] absolute bottom-0' />
           </div>
           {/* second col */}
           <div className='py-10 pl-5 bg-gradient-to-br from-brandYellow to-brandYellow/90 text-white rounded-3xl relative h-[320px] flex items-end'>
@@ -48,7 +48,7 @@ export default function Category() {
                 />
               </div>
             </div>
-            <img data-aes="zoom-out" src={Image2} alt="" className='w-[320px] absolute -right-4 lg:top-[40px]' />
+            <img data-aos="zoom-in" src={Image2} alt="" className='w-[320px] absolute -right-4 lg:top-[40px]' />
           </div>
           {/* third col */}
           <div className='sm:col-span-2 py-10 pl-5 bg-gradient-to-br from-primary to-primary/90 text-white rounded-3xl relative h-[320px] flex items-end'>
@@ -68,7 +68,7 @@ export default function Category() {
                 />
               </div>
             </div>
-            <img data-aos="zoom-out" src={Image3} alt="" className='w-[320px] absolute -right-4 lg:top-[40px]' />
+            <img data-aos="zoom-in" src={Image3} alt="" className='w-[250px] absolute top-1/1 -translate-y-1/1 -right-0' />
           </div>
         </div>
       </div>
--- src/components/Category/Category2.jsx
+++ src/components/Category/Category2.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-import Image1 from "../../assets/category/earphone.png";
-import Image2 from "../../assets/category/watch.png";
-import Image3 from "../../assets/category/macbook.png";
+import Image1 from "../../assets/category/gaming.png";
+import Image2 from "../../assets/category/vr.png";
+import Image3 from "../../assets/category/speaker.png";
 import Button from '../Shared/Button';
 
 export default function Category2() {
@@ -11,64 +11,52 @@ export default function Category2() {
       <div className='container'>
         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8'>
           {/* first col */}
-          <div className='py-10 pl-5 bg-gradient-to-br from-black/90 to-black/70 text-white rounded-3xl relative h-[320px] flex items-end'>
-            <div
-              data-aos="fade-up"
-              data-aos-delay="10"
-            >
+          <div className='sm:col-span-2 py-10 pl-5 bg-gradient-to-br from-gray-400/90 to-gray-100 text-white rounded-3xl relative h-[320px] flex items-end"'>
+            <div>
               <div className='mb-4'>
-                <p data-aos="zoom-in" className='mb-[2px] text-gray-400'>Enjoy</p>
-                <p data-aos="zoom-in" className='text-2xl font-semibold mb-[2px]'>With</p>
-                <p data-aos="zoom-in" className='text-4xl xl:text-5xl font-bold opacity-20 mb-2'>Earphone</p>
+                <p className='mb-[2px] text-white'>Enjoy</p>
+                <p className='text-2xl font-semibold mb-[2px]'>With</p>
+                <p className='text-4xl xl:text-5xl font-bold opacity-40 mb-2'>Gaming</p>
                 <Button
                   text="Browse"
                   bgColor="bg-primary"
                   textColor="text-white"
-                  data-aos="zoom-out"
                 />
               </div>
             </div>
-            <img src={Image1} alt="" className='w-[320px] absolute bottom-0' />
+            <img data-aos="zoom-in" src={Image1} alt="" className='w-[250px] absolute top-1/1 -translate-y-1/2 -right-0' />
           </div>
           {/* second col */}
-          <div className='py-10 pl-5 bg-gradient-to-br from-brandYellow to-brandYellow/90 text-white rounded-3xl relative h-[320px] flex items-end'>
+          <div className='py-10 pl-5 bg-gradient-to-br from-brandGreen/90 to-brandGreen/90 text-white rounded-3xl relative h-[320px] flex items-start'>
             <div>
-              <div
-                data-aos="fade-up"
-                data-aos-delay="10"
-                className='mb-4'>
-                <p data-aos="zoom-in" className="mb-[2px] text-white">Enjoy</p>
-                <p data-aos="zoom-in" className="text-2xl font-semibold mb-[2px]">With</p>
-                <p data-aos="zoom-in" className="text-4xl xl:text-5xl font-bold opacity-40 mb-2">Gadget</p>
+              <div className='mb-4'>
+                <p class="mb-[2px] text-white">Enjoy</p>
+                <p class="text-2xl font-semibold mb-[2px]">With</p>
+                <p class="text-4xl xl:text-5xl font-bold opacity-40 mb-2">Visual</p>
                 <Button
                   text="Browse"
-                  bgColor="bg-primary"
-                  textColor="text-white"
-                  data-aos="zoom-out"
+                  bgColor="bg-white"
+                  textColor="text-brandGreen"
                 />
               </div>
             </div>
-            <img data-aes="zoom-out" src={Image2} alt="" className='w-[320px] absolute -right-4 lg:top-[40px]' />
+            <img data-aos="zoom-in" src={Image2} alt="" className='w-[320px] absolute -right-4 lg:top-[40px]' />
           </div>
           {/* third col */}
-          <div className='sm:col-span-2 py-10 pl-5 bg-gradient-to-br from-primary to-primary/90 text-white rounded-3xl relative h-[320px] flex items-end'>
+          <div className='py-10 pl-5 bg-gradient-to-br from-brandBlue to-brandBlue/90 text-white rounded-3xl relative h-[320px] flex items-start'>
             <div>
-              <div
-                data-aos="fade-up"
-                data-aos-delay="10"
-                className='mb-4'>
-                <p data-aos="zoom-in" className="mb-[2px] text-white">Enjoy</p>
-                <p data-aos="zoom-in" className="text-2xl font-semibold mb-[2px]">With</p>
-                <p data-aos="zoom-in" className="text-4xl xl:text-5xl font-bold opacity-40 mb-2">Laptop</p>
+              <div className='mb-4'>
+                <p class="mb-[2px] text-white">Enjoy</p>
+                <p class="text-2xl font-semibold mb-[2px]">With</p>
+                <p class="text-4xl xl:text-5xl font-bold opacity-40 mb-2">Speaker</p>
                 <Button
                   text="Browse"
-                  bgColor="bg-white text-primary cursor-pointer hover:scale-105 duration-300 py-2 px-8 rounded-full relative z-10"
-                  textColor="text-primary"
-                  data-aos="zoom-out"
+                  bgColor="bg-white"
+                  textColor="text-brandBlue"
                 />
               </div>
             </div>
-            <img data-aos="zoom-out" src={Image3} alt="" className='w-[320px] absolute -right-4 lg:top-[40px]' />
+            <img data-aos="zoom-in" src={Image3} alt="" className='class="w-[250px] absolute top-1/1 -translate-y-1/2 -right-0"' />
           </div>
         </div>
       </div>
